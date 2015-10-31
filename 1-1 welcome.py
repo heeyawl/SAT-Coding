a71e7dce5354fe879783f0915818ba187815b004
Heeyawl Yu <heeyawl@gmail.com>
2015-10-31 15:48:47 +0900

    1부터 1000까지의 합

--- 1-1 welcome.py
+++ 1-1 welcome.py
@@ -1,2 +1,6 @@
-print "Welcome to Python!"
-print "Welcome to Python!"
+# -*- coding: utf-8 -*-
+print("Welcome to Python!")
+print("Welcome to Python!")
+
+print("1부터 1000까지의 합")
+print(sum(range(1, 1001)))
